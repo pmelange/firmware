5cf19cd3760ff36d19c527d8b3a1512e1147e1aa
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-02-16 01:40:09 +0100

    Update OpenWrt to HEAD of lede-17.01 branc
    f3f21b3d2a mac80211: brcmfmac: fix a possible NULL pointer dereference

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=6c14b29bf1c28ca2cb171b1a46d1c880c0794f1d
+OPENWRT_COMMIT=f3f21b3d2a7430cbd39c7e8ec04165e9efcac8ba
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
