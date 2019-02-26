87763581e195ed9f4b015727843b2805eee5da7c
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-02-27 00:30:17 +0100

    Update OpenWrt to HEAD of lede-17.01 branc
    fb333a7fec mac80211: brcmfmac: pick few 4.17 cleanups required for further fixes
    eab6537449 mac80211: brcmfmac: backport 5.0 & 5.1 important changes/fixes

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=f3f21b3d2a7430cbd39c7e8ec04165e9efcac8ba
+OPENWRT_COMMIT=eab65374490a53dd88bdff00be11cdf1fcb5fe62
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
