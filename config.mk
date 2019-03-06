0d65c5f392b1c57af98fc8f8ab89f4aa90607acd
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-07 00:30:16 +0100

    Update OpenWrt to HEAD of lede-17.01 branc
    981f5f7e40 kernel: fix refcnt leak in LED netdev trigger on interface rename

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=eab65374490a53dd88bdff00be11cdf1fcb5fe62
+OPENWRT_COMMIT=981f5f7e40cbf26bc0beb0a2aa5f3c562b83c85b
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
