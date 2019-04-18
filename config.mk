5a91769375ac1008e969d8038292d5460159288d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-19 00:30:13 +0200

    Update OpenWrt to HEAD of lede-17.01 branc
    eaef74279c mac80211: brcmfmac: early work on FullMAC firmware crash recovery
    d3bab051cf mac80211: brcmfmac: really add early fw crash recovery

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=981f5f7e40cbf26bc0beb0a2aa5f3c562b83c85b
+OPENWRT_COMMIT=d3bab051cfd1f2f5791e4352309c51f5e29b678f
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
