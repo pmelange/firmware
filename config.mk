8de453784c36268549947251b4c391b768480e49
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-23 00:30:13 +0200

    Update OpenWrt to HEAD of lede-17.01 branc
    3e7fd9275d ca-certificates: Update to Version 20180409
    e74b264231 ca-certificates: ca-bundle: add symlink for openssl default setting
    4f23857011 ca-caertificates: remove myself as PKG_MAINTAINER
    c2f938bf9a ca-certificates: update to version 20190110
    c476954633 openssl: bump to 1.0.2r

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d3bab051cfd1f2f5791e4352309c51f5e29b678f
+OPENWRT_COMMIT=c476954633887d8a1fcd80482e821074d7c4a36e
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
