f479965f2b66c47fe740ec162e0bd44b7238e22c
pmelange <isprotejesvalkata@gmail.com>
2019-06-18 11:09:01 +0200

    add target, platform, and device support for easybox 904xDSL

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
-OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=158a71621577c6e52dc8539a773ba62e93ed5a1f
+OPENWRT_SRC=https://github.com/Plonkbong/openwrt.git
+OPENWRT_COMMIT=589db30055c0a7768601832c5006f808aea57711
 SET_BUILDBOT=env
 MAKE_ARGS=V=s
